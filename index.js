97342a48dcbca7ddba3c85ad206842a644ad0d3a
Aaron Hellam <aaronhellam@gmail.com>
2022-02-28 14:34:25 -0800

    commit tree c773238611734e9f9b481fea5bab21e87569df18

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-const mainElement = document.querySelector('#main');
-mainElement.remove();
 
-const newHeader = document.createElement('h1');
-newHeader.id = "victory";
-newHeader.innerHTML = "Aaron is the champion."
+const mainElement = document.querySelector('#main');
+mainElement.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'AARON-HELLAM is the champion'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mainElement = document.querySelector('#main');
+// mainElement.remove();
+
+// const newHeader = document.createElement('h1');
+// newHeader.id = "victory";
+// newHeader.innerHTML = "Aaron is the champion."
